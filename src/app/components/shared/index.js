49b56c79d50ed6f46a24f220871247ae7d2ce1af
Dhananjaya <43692598+DhananjayaRB@users.noreply.github.com>
2025-08-14 11:20:00 +0530

    Delete src directory

--- src/app/components/shared/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Navbar } from './navbar';
-export { default as Footer } from './footer';
-export { default as ScrollToTop } from './scrollToTop';
-export { default as LoadingSpinner } from './loadingSpinner';
-
